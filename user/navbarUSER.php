f2fa298c9dc0814112e25dde78533520ddd9aaaa
Suryo Nugroho <suryoo.nug@gmail.com>
2024-02-06 02:34:03 +0700

    Update navbarUSER.php

--- user/navbarUSER.php
+++ user/navbarUSER.php
@@ -7,7 +7,7 @@
  <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #50C878">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="halaman_user.php">MASTER <span style="color:#008000">KOMPUTER</span></a>
+            <a class="navbar-brand" href="halaman_user.php">Gass <span style="color:#008000">KOMPUTER</span></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
