adfe69cc3d9390595139ed50b5a5b0fcbefea843
Suryo Nugroho <suryoo.nug@gmail.com>
2024-02-29 20:41:54 +0700

    Update bluehorizon.sql

--- assets/db/bluehorizon.sql
+++ assets/db/bluehorizon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2024 at 03:39 PM
+-- Generation Time: Feb 29, 2024 at 02:41 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -55,13 +55,23 @@ CREATE TABLE `payment` (
   `notelp_pl` varchar(15) NOT NULL,
   `nama_pl` varchar(30) NOT NULL,
   `norek_pl` varchar(20) NOT NULL,
+  `email_pl` varchar(50) NOT NULL,
   `checkin` date NOT NULL,
   `checkout` date NOT NULL,
   `jumlah_tamu` int(100) NOT NULL,
-  `tipe_room` varchar(10) NOT NULL,
   `id_room` varchar(10) NOT NULL,
   `harga_total` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`id_payment`, `notelp_pl`, `nama_pl`, `norek_pl`, `email_pl`, `checkin`, `checkout`, `jumlah_tamu`, `id_room`, `harga_total`) VALUES
+(26, '212123', 'Rahman', '2123123123', 'rahman.nugroohoo@gmail.com', '2024-02-29', '2024-03-01', 1, 'BR001', 500000),
+(27, '212123', 'Rahman2', '2123123123', 'rahman.nugroohoo2@gmail.com', '2024-02-29', '2024-03-01', 1, 'DR001', 1000000),
+(28, '212123', 'Rahman3', '2123123123', 'rahman.nugroohoo3@gmail.com', '2024-02-29', '2024-03-01', 1, 'PR001', 1500000),
+(29, '212123', 'Rahman4', '2123123123', 'rahman.nugroohoo4@gmail.com', '2024-02-29', '2024-03-01', 1, 'HR001', 2500000);
 
 -- --------------------------------------------------------
 
@@ -93,31 +103,31 @@ CREATE TABLE `room` (
 --
 
 INSERT INTO `room` (`id_room`, `tipe_room`, `harga_room`, `status`) VALUES
-('BR001', 'Basic', 500000, 'Available'),
+('BR001', 'Basic', 500000, 'Booked'),
 ('BR002', 'Basic', 500000, 'Available'),
 ('BR003', 'Basic', 500000, 'Available'),
 ('BR004', 'Basic', 500000, 'Available'),
 ('BR005', 'Basic', 500000, 'Available'),
-('DR001', 'Daily', 1000000, 'Available'),
+('DR001', 'Daily', 1000000, 'Booked'),
 ('DR002', 'Daily', 1000000, 'Available'),
 ('DR003', 'Daily', 1000000, 'Available'),
 ('DR004', 'Daily', 1000000, 'Available'),
 ('DR005', 'Daily', 1000000, 'Available'),
-('PR001', 'Panoramic', 1500000, 'Available'),
-('PR002', 'Panoramic', 1500000, 'Available'),
-('PR003', 'Panoramic', 1500000, 'Available'),
-('PR004', 'Panoramic', 1500000, 'Available'),
-('PR005', 'Panoramic', 1500000, 'Available'),
 ('ER001', 'Exclusive', 2000000, 'Available'),
 ('ER002', 'Exclusive', 2000000, 'Available'),
 ('ER003', 'Exclusive', 2000000, 'Available'),
 ('ER004', 'Exclusive', 2000000, 'Available'),
 ('ER005', 'Exclusive', 2000000, 'Available'),
-('HR001', 'Honey', 2500000, 'Available'),
+('HR001', 'Honey', 2500000, 'Booked'),
 ('HR002', 'Honey', 2500000, 'Available'),
 ('HR003', 'Honey', 2500000, 'Available'),
 ('HR004', 'Honey', 2500000, 'Available'),
-('HR005', 'Honey', 2500000, 'Available');
+('HR005', 'Honey', 2500000, 'Available'),
+('PR001', 'Panoramic', 1500000, 'Booked'),
+('PR002', 'Panoramic', 1500000, 'Available'),
+('PR003', 'Panoramic', 1500000, 'Available'),
+('PR004', 'Panoramic', 1500000, 'Available'),
+('PR005', 'Panoramic', 1500000, 'Available');
 
 --
 -- Indexes for dumped tables
@@ -133,13 +143,21 @@ ALTER TABLE `login`
 -- Indexes for table `payment`
 --
 ALTER TABLE `payment`
-  ADD PRIMARY KEY (`id_payment`);
+  ADD PRIMARY KEY (`id_payment`),
+  ADD KEY `id_room` (`id_room`),
+  ADD KEY `norek_pl` (`norek_pl`);
 
 --
 -- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`norek_pl`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id_room`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -155,7 +173,17 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `id_payment` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_payment` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`id_room`) REFERENCES `room` (`id_room`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
